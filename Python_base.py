362437ad37e901ff6875503c51b82984cc592430
Shypilov Dmitriy <shypilovd@gmail.com>
2022-04-28 16:04:46 +0300

    main basis of Python
    *data types
    *some built-in functions
    *string methods
    *function ord(a)-98

---
+++ Python_base.py
@@ -0,0 +1,28 @@
+# data types in Python
+list_of_immutable_types = ['int', 'float', 'str', 'tuple', 'frozen set','bool']
+list_of_mutable_types = ['list', 'dict', 'set']
+
+# math operations
+math_op_list = ['*', '/', '//', '%', '+', '-']
+
+# built-in math function
+list_of_builtin_functions = ['min', 'max', 'abs', 'pow', 'round']
+
+# print('1', '2', sep = 'separator', end = '\n')
+
+#import math, trunc(), floor(), ceil()
+
+comparison_operators= ['>', '<', '==', '!=', '<=', '>=']
+# not True - False, a and b - True, a or b
+
+str_operations = ['concatinations', 'Multiplication', 'len()', 'in', '> or <','[index]' , '[::0]']
+# ord('b') - 98 ,
+
+methods_of_strings =['a.upper()', 'a.lower()', 'a.count(sub,start,end)', 'a.find(sub, start)', 'a.rfind(sub, start)',
+                     'a.index(sub, start)','a.rindex(sub, start)', 'a.replace(old,new,count)', 'a.isalpha()', 'a.isdigit()',
+                     'a.rjust(width, fillchar)', 'a.ljust(width, fillchar)', 'a.split(sep,maxsplit)', '\'separator\'.join(our list)',
+                     'a.strip()', 'a.lstrip()', 'a.rstrip()', 'a.startswith(prefix)', 'a.swapcase()', 'a.partition(sep)',
+                     'a.center(width, fillchar)', 'a.capitalize()', 'a.isspace()', 'a.capitalize()', 'a.istitle()',
+                     'a.isalnum()', 'a.isidentifier()', 'a.endswith(prefix)', 'a.islower()', 'a.isupper()','a.zfill(width)',
+                     'a.title()', 'a.isnumeric()']
+#find return -1, index call exclusion
