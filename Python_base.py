0cc48138a98aea793008ddf450e42bf55f6733cc
Shypilov Dmitriy <shypilovd@gmail.com>
2022-04-28 17:15:09 +0300

    Added list basic operation

--- Python_base.py
+++ Python_base.py
@@ -1,28 +1,47 @@
 # data types in Python
-list_of_immutable_types = ['int', 'float', 'str', 'tuple', 'frozen set','bool']
+list_of_immutable_types = ['int', 'float', 'str', 'tuple', 'frozen set', 'bool']
 list_of_mutable_types = ['list', 'dict', 'set']
 
 # math operations
 math_op_list = ['*', '/', '//', '%', '+', '-']
 
 # built-in math function
-list_of_builtin_functions = ['min', 'max', 'abs', 'pow', 'round']
+list_of_builtin_functions = ['min', 'max', 'abs', 'pow', 'round', 'sum']
 
 # print('1', '2', sep = 'separator', end = '\n')
 
-#import math, trunc(), floor(), ceil()
+# import math, trunc(), floor(), ceil()
 
-comparison_operators= ['>', '<', '==', '!=', '<=', '>=']
+comparison_operators = ['>', '<', '==', '!=', '<=', '>=']
 # not True - False, a and b - True, a or b
 
-str_operations = ['concatinations', 'Multiplication', 'len()', 'in', '> or <','[index]' , '[::0]']
+str_operations = ['concatinations', 'Multiplication', 'len()', 'in', '> or <', '[index]', '[::0]']
 # ord('b') - 98 ,
 
-methods_of_strings =['a.upper()', 'a.lower()', 'a.count(sub,start,end)', 'a.find(sub, start)', 'a.rfind(sub, start)',
-                     'a.index(sub, start)','a.rindex(sub, start)', 'a.replace(old,new,count)', 'a.isalpha()', 'a.isdigit()',
-                     'a.rjust(width, fillchar)', 'a.ljust(width, fillchar)', 'a.split(sep,maxsplit)', '\'separator\'.join(our list)',
-                     'a.strip()', 'a.lstrip()', 'a.rstrip()', 'a.startswith(prefix)', 'a.swapcase()', 'a.partition(sep)',
-                     'a.center(width, fillchar)', 'a.capitalize()', 'a.isspace()', 'a.capitalize()', 'a.istitle()',
-                     'a.isalnum()', 'a.isidentifier()', 'a.endswith(prefix)', 'a.islower()', 'a.isupper()','a.zfill(width)',
-                     'a.title()', 'a.isnumeric()']
-#find return -1, index call exclusion
+methods_of_strings = ['a.upper()', 'a.lower()', 'a.count(sub,start,end)', 'a.find(sub, start)', 'a.rfind(sub, start)',
+                      'a.index(sub, start)', 'a.rindex(sub, start)', 'a.replace(old,new,count)', 'a.isalpha()',
+                      'a.isdigit()',
+                      'a.rjust(width, fillchar)', 'a.ljust(width, fillchar)', 'a.split(sep,maxsplit)',
+                      '\'separator\'.join(our list)',
+                      'a.strip()', 'a.lstrip()', 'a.rstrip()', 'a.startswith(prefix)', 'a.swapcase()',
+                      'a.partition(sep)',
+                      'a.center(width, fillchar)', 'a.capitalize()', 'a.isspace()', 'a.capitalize()', 'a.istitle()',
+                      'a.isalnum()', 'a.isidentifier()', 'a.endswith(prefix)', 'a.islower()', 'a.isupper()',
+                      'a.zfill(width)',
+                      'a.title()', 'a.isnumeric()']
+# find return -1, index call exclusion
+
+'''
+f string, can use {3*23}, {function}
+a = 'some {0} text {1}'.format(1, 3)
+b = 'some {b} text {a}'.format(a = 'here', b = 'my')
+i= 'apples'
+c = f'I like {i}'
+d = {'name':'Dmitriy', 'age': 35}
+text1 = f'My name is {d["name"]}, age is {d["age"]}'
+'''
+
+list_operations = ['add list + list', 'Multiplication list * number', 'len()', 'in', '> or < for the same type',
+                   '[index]', '[::0]', 'sorted(list,key, reverse =True or False)', 'a.append(object)', 'a.clear()',
+                   'a.copy()', 'a.count(object)', 'a.extend(sequence)', 'a.index(value, start, stop)', 'a.insert(index, object)',
+                   'a.pop(index)', 'a.remove(value)', 'a.reverse()', 'a.sort(key=None, reverse=True)', ]
