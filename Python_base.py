4b03b6029620b75af25cc48f83d826c8e01fe3df
Shypilov Dmitriy <shypilovd@gmail.com>
2022-04-28 19:41:28 +0300

    conditionals, loops

--- Python_base.py
+++ Python_base.py
@@ -43,5 +43,27 @@ text1 = f'My name is {d["name"]}, age is {d["age"]}'
 
 list_operations = ['add list + list', 'Multiplication list * number', 'len()', 'in', '> or < for the same type',
                    '[index]', '[::0]', 'sorted(list,key, reverse =True or False)', 'a.append(object)', 'a.clear()',
-                   'a.copy()', 'a.count(object)', 'a.extend(sequence)', 'a.index(value, start, stop)', 'a.insert(index, object)',
-                   'a.pop(index)', 'a.remove(value)', 'a.reverse()', 'a.sort(key=None, reverse=True)', ]
+                   'a.copy()', 'a.count(object)', 'a.extend(sequence)', 'a.index(value, start, stop)',
+                   'a.insert(index, object)', 'a.pop(index)', 'a.remove(value)', 'a.reverse()', 'a.sort(key=None, reverse=True)', ]
+# conditionals
+if 5 > 10:
+    print('True')
+elif 7 < 3:
+    print('True')
+else:
+    pass
+
+#loops
+
+#while True:    (a !=0, 3 in list , len(a)> 0)
+#   print(1)
+#else:
+#   print(2)
+#  operator break, instructions continue, if elif else statement
+
+# function range, create object of class range, can be use with build-in function iter which returns an iterator
+# also can use with list(), tuple(), set()
+v1 = range(1,10,2)
+v1 = list(range(1,10,2))
+#!!!!!  list, tuple, set are not iterator, can't use functions next(), and __next__
+
