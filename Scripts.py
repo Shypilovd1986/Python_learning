98355b0d9ac3454c160500115c92b6eeea67e046
Shypilov Dmitriy <shypilovd@gmail.com>
2022-04-29 10:12:49 +0300

    add function for building matrix

--- Scripts.py
+++ Scripts.py
@@ -11,10 +11,37 @@ def algorithm_of_Euklid():
     else:
         print(f'The lowest common multiple of {a1} and {b1} is {a}')
 
+
 # find factorial of number
 def fact_of_number(a):
     # a = int(input('input number for finding it\'s factorial\n'))
     f = 1
-    for i in range(1,a+1):
-        f*=i
+    for i in range(1, a + 1):
+        f *= i
     return f
+
+
+def check_list():
+    a = input('type in numbers').split()
+    b = []
+    for i in a:
+        if i not in b:
+            b.append(i)
+    return b
+
+
+# build matrix with main diagonal (nested lists)
+def build_matrix():
+    a = int(input('Input quantity of rows '))
+    b = int(input('Input quantity of columns '))
+    list = []
+    for i in range(a):
+        list.append([])
+        for j in range(b):
+            if i < j:
+                list[i].append(3)
+            elif i == j:
+                list[i].append(0)
+            else:
+                list[i].append(7)
+    return list
