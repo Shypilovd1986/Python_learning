8f91e7227686c5390a314095f7bc971c0a0e0429
Shypilov Dmitriy <shypilovd@gmail.com>
2022-06-19 22:51:10 +0300

    json

--- Describe_Modules.py
+++ Describe_Modules.py
@@ -9,3 +9,29 @@
 # os.mkdir('C:\\m1')    -   create file m1
 # print(os.path.exists('C:\\m1'))   return True if file exists or False if not
 
+# *************      json       *****************
+# import json
+# from random import randint
+
+# JSON  -JavaScript Object Notation, A common use of JSON is to read data from a web server, and display the data in a
+# web page. Применяется в веб-приложениях как для обмена данными между браузером и сервером (AJAX), так и между
+# серверами (программные HTTP-сопряжения).
+# !!!!!!!  use only double quotes not single
+# str_json = строка из джейсона
+# data = json.loads(str_json)   - переведет в словарь
+# new_json = json.dumps(data, indent=2) переведет словарь в строку
+
+
+# with open('my.json', 'w') as file:
+#     json.dump(new_json, file, indent=2)    создаст и запишет в файл джейсон нашу строку
+# with open('my.json', 'r') as file:
+#     data1 = json.load(file)               считает нашу строку из джейсона
+# !!!!  decoding JSON -> Python
+# object -> dict
+# array ->list
+# string -> str
+# number(int) -> int
+# number(real) -> float
+# true -> True
+# false -> False
+# null -> None
