0a1faaeead4946f1e8aa562676eb4e4153ab8379
Shypilov Dmitriy <shypilovd@gmail.com>
2022-06-19 15:07:30 +0300

    module os

--- Describe_Modules.py
+++ Describe_Modules.py
@@ -4,3 +4,8 @@
 # random()  returns random number from 0 to  1
 # shuffle(a), shuffles mutable sequence a
 #
+# ************   os     *************
+# import os
+# os.mkdir('C:\\m1')    -   create file m1
+# print(os.path.exists('C:\\m1'))   return True if file exists or False if not
+
