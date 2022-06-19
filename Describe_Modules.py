9b593f67a4b6473d6376dfeb2dd79a9113cba9dc
Shypilov Dmitriy <shypilovd@gmail.com>
2022-06-19 14:53:32 +0300

    module random

--- Describe_Modules.py
+++ Describe_Modules.py
@@ -1,3 +1,6 @@
-#module random
-from random import randint
-randint(1,200) # takes random numbers from range of values 1 - 200
+# ***********    module random    ***********
+# from random import randint
+# randint(1,200)  takes random numbers from range of values 1 - 200
+# random()  returns random number from 0 to  1
+# shuffle(a), shuffles mutable sequence a
+#
