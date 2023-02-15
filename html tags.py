8813ba2e28888c465f39aa8f324d7854866881b6
shypilovd1986 <shypilovd@gmail.com>
2023-02-15 23:55:52 +0200

    popular html tags

---
+++ html tags.py
@@ -0,0 +1,42 @@
+# <!doctype html>
+# <html lang="en">
+# <head>
+#     <meta charset="UTF-8">
+#     <meta name="viewport"
+#           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+#     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+#     <meta name="author" content="Dmitriy Shypilov">
+#     <meta name="keywords" content="test, html">
+#     <title>Document</title>
+# </head>
+# <body>
+#     <!-- comments -->
+#     <h1>Big header</h1>
+#     <h2>Header</h2>
+#     <h3>Header</h3>
+#     <h4>Header</h4>
+#     <h5>Header</h5>
+#     <h6>Header</h6>
+#     <p title="some text">  Lorem ipsum dolor sit <b> amet, </b> consectetur <i> adipisicing </i>elit. <strike> Aperiam
+#         architecto </strike>
+#         commodi culpa <strong> cumque dolores </strong>, ex non officiis <sup>perferendis</sup> <sub> sequi </sub>
+#         tempora. </p>
+#     <p> Lorem <small> ipsum dolor </small> sit amet, <big>una matina </big> adipisicing elit. Tempora, totam.</p>
+#     <pre>
+#         def main():
+#             x=10
+#             return x
+#     </pre>
+#     <code>some code </code>
+#     <ul type="square">
+#         <li>apple</li>
+#         <li>pineapple</li>
+#         <li>plum</li>
+#     </ul>
+#     <ol type="I" start="II" >
+#         <li>apple</li>
+#         <li>pineapple</li>
+#     </ol>
+#
+# </body>
+# </html>
