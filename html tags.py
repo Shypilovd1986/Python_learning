f468cd5aeff03d8f6b5567b1dbe01b1237d2d63b
shypilovd1986 <shypilovd@gmail.com>
2023-02-16 22:25:55 +0200

    learn more html tags

--- html tags.py
+++ html tags.py
@@ -10,24 +10,29 @@
 #     <title>Document</title>
 # </head>
 # <body>
-#     <!-- comments -->
+#     <!-- Headers -->
 #     <h1>Big header</h1>
 #     <h2>Header</h2>
 #     <h3>Header</h3>
 #     <h4>Header</h4>
 #     <h5>Header</h5>
 #     <h6>Header</h6>
+#     <a href="#bottom">Go to bottom of page</a>
 #     <p title="some text">  Lorem ipsum dolor sit <b> amet, </b> consectetur <i> adipisicing </i>elit. <strike> Aperiam
 #         architecto </strike>
 #         commodi culpa <strong> cumque dolores </strong>, ex non officiis <sup>perferendis</sup> <sub> sequi </sub>
 #         tempora. </p>
 #     <p> Lorem <small> ipsum dolor </small> sit amet, <big>una matina </big> adipisicing elit. Tempora, totam.</p>
+#     <!-- show code without format -->
 #     <pre>
 #         def main():
 #             x=10
 #             return x
 #     </pre>
-#     <code>some code </code>
+#     <code>
+#         def main(x):
+#             return x*3
+#     </code>
 #     <ul type="square">
 #         <li>apple</li>
 #         <li>pineapple</li>
@@ -37,6 +42,15 @@
 #         <li>apple</li>
 #         <li>pineapple</li>
 #     </ol>
+#     <table>
+#
+#     </table>
+#     <a href="http:\\google.com" target="_blank">Go to Google</a><br>
+#     <a href="list_of_item.html">Список товаров</a><br>
+#     <a name="bottom"></a>
+#     <!-- anchor link -->
+#     <a href="mailto:shypilovd@gmail.com">Send to message</a><br>
+#    <img src="images\house.jpg " width="500" alt="house" height="auto">
 #
 # </body>
 # </html>
