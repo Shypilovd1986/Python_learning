eec7c4263731014f55787eb5b6270ee57b4a01d8
shypilovd1986 <shypilovd@gmail.com>
2023-02-18 12:52:47 +0200

    End course HTML

--- html tags.py
+++ html tags.py
@@ -7,6 +7,7 @@
 #     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 #     <meta name="author" content="Dmitriy Shypilov">
 #     <meta name="keywords" content="test, html">
+#     <link rel="stylesheet" href="css/main.css">
 #     <title>Document</title>
 # </head>
 # <body>
@@ -42,15 +43,88 @@
 #         <li>apple</li>
 #         <li>pineapple</li>
 #     </ol>
-#     <table>
+#     <table bgcolor="red" border="1" width="200px">
+#         <thead>
+#             <tr>
+#                 <th>Apple</th>
+#                 <th>Plum</th>
+#                 <th>Pear</th>
+#             </tr>
+#         </thead>
+#         <tbody align="center">
+#             <tr>
+#                 <td>23</td>
+#                 <td>354</td>
+#                 <td>1111</td>
+#             </tr>
+#         </tbody>
+#         <tfoot>
 #
+#         </tfoot>
 #     </table>
 #     <a href="http:\\google.com" target="_blank">Go to Google</a><br>
 #     <a href="list_of_item.html">Список товаров</a><br>
 #     <a name="bottom"></a>
 #     <!-- anchor link -->
-#     <a href="mailto:shypilovd@gmail.com">Send to message</a><br>
-#    <img src="images\house.jpg " width="500" alt="house" height="auto">
 #
+#     <a href="mailto:shypilovd@gmail.com">Send to message</a><br>
+#
+#     <img src="images\house.jpg " width="500" alt="house" height="auto">
+#     <script src="js/main.js"></script>
+#     <div>Container</div>
+#     <p>Lorem ipsum dolor sit amet, <span>consectetur adipisicing elit</span>. Cupiditate, quidem.</p>
+#
+#     <form action="" method="" name="user_information" id="1">
+#         <label>Input your name</label><br>
+#         <input type="text" placeholder="Dmitriy" value="some" maxlength="20" required> <br>
+#
+#         <input type="text" value="Your info" disabled><br>
+#         <input type="url" value="google.com" readonly><br>
+#
+#         <label>Input your email</label><br>
+#         <input type="email"><br>
+#         <hr>
+#
+#         <input type="button" value="click"><br>
+#         <input type="date" value="click"><br>
+#         <input type="color" value="click"><br>
+#         <input type="radio" name="1">
+#         <input type="radio" name="1"><br>
+#         <input type="radio" name="1"><br>
+#         <input type="checkbox"><br>
+#         <input type="search"><br>
+#         <input type="password"><br>
+#         <input type="submit">
+#     </form>
+#
+#     <form action="" method="" name="text">
+#         <label>Input your text</label><br>
+#         <textarea placeholder="your favorite poem" maxlength="200">
+#         </textarea><br>
+#         <button type="submit" >Send text</button>
+#     </form>
+#
+#     <form action="" method="" name="categories">
+#         <label>Choose your category</label><br>
+#         <select>
+#             <!--<select multiple size="2">  your can choose 2 position-->
+#             <option>Electricity</option>
+#             <option>Home</option>
+#             <option >Plumbing</option>
+#             <option selected disabled>-------</option>
+#         </select>
+#     </form>
+#
+#     <!-- html5 tags-->
+#     <header></header>
+#     <main></main>
+#     <aside></aside>
+#     <footer></footer>
+#     <progress max="100" value="9">install in 9 percents</progress>
+#     <details >
+#         <summary>Your details</summary>
+#         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum, vitae!
+#     </details>
+#     <a target="_blank" href="https://www.browserstack.com/?utm_source=google&utm_medium=cpc&utm_platform=paidads&utm_content=434390274153&utm_campaign=Search-Brand-Tier1-EMEA-CL&utm_campaigncode=BrowserStack-Alpha+1012866&utm_term=e+browserstack&gclid=Cj0KCQiAi8KfBhCuARIsADp-A54WSPR7ehUxB5mkdgKC7DU_fzDyvau6UoBgwFXr5oUQQaBdtL4xc6EaAgCvEALw_wcB">Testing your site</a>
 # </body>
 # </html>
