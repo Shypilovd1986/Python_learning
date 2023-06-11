f60430adacf416d6b76aa8bb1b224c1caa52a934
shypilovd1986 <shypilovd@gmail.com>
2023-06-11 22:19:32 +0300

    agragate functions MYSQL

--- SQL_course_LinkedIn.py
+++ SQL_course_LinkedIn.py
@@ -28,4 +28,24 @@
 #
 # SELECT COUNT(sb_id) AS count_taken_book FROM subscriptions;
 
-#
+# SELECT MIN(sb_finish) FROM subscriptions;
+
+# SELECT b_name, b_year FROM books ORDER BY b_year  ASC;   or DESC
+
+# compound conditions
+# SELECT b_year, b_name, b_quantity FROM books WHERE b_year BETWEEN 1990 AND 2000 AND b_quantity >= 3;
+
+# SELECT b_year, b_name, b_quantity FROM books WHERE b_year >= 1990 AND b_year <= 2000 AND b_quantity >= 3;
+# double inequality
+
+# SELECT b_id, b_quantity FROM books WHERE b_quantity < (SELECT AVG(b_quantity) FROM books);
+
+#!!!! When you need to use an aggregate function inside a WHERE clause, you need to wrap the aggregate function call in
+# a subquery.
+
+# SELECT sb_id, sb_book FROM subscriptions WHERE sb_book = (SELECT MAX(sb_book) FROM subscriptions);
+
+# SELECT AVG(DATEDIFF(sb_finish, sb_start)) AS avg_days FROM subscriptions WHERE sb_is_active='N';
+
+# CURDATE()   function show current day
+# YEAR('some date')  extract year from date
