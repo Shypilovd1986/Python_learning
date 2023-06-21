c9b0f4a0f5f0cee15be0e2321e4c52579dcdbfc1
shypilovd1986 <shypilovd@gmail.com>
2023-06-21 23:01:25 +0300

    JSON start course, serializing data

---
+++ JSON.py
@@ -0,0 +1,5 @@
+#  Serializing JSON means turning a structure into a JSON string, while deserializing is the reverse, turning a JSON
+#  string into a structure that a programming language can work with. Remember that even though JSON is based on the
+#  object notation used in JavaScript, JSON can be used across languages and platforms.
+
+#
