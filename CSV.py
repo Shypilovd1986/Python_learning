9b7df2452dd17f1355bd39dfcc3e555d090263be
shypilovd1986 <shypilovd@gmail.com>
2023-06-22 18:26:49 +0300

    CSV JSON format data

---
+++ CSV.py
@@ -0,0 +1,18 @@
+# import CSV
+#
+# with open('list.csv', 'r') as f:
+#     reader = csv.reader(f)
+#     for row in reader:  reader[2:]
+#         print(row)
+
+# or
+
+# reader = csv.DictReader(f, delimiter='\t')
+# for row in reader:  reader[2:]
+    # print(row)
+
+
+# with open('list.csv', 'w') as f:
+#     writer = csv.writer(f)
+#     for row in data:
+#         writer.writerow(row)
--- JSON.py
+++ JSON.py
@@ -2,4 +2,15 @@
 #  string into a structure that a programming language can work with. Remember that even though JSON is based on the
 #  object notation used in JavaScript, JSON can be used across languages and platforms.
 
-#
+# YAML Yet Another Markup Language
+# XML Extensible Markup Language
+
+# import json
+# from json import JSONDecodeError
+
+# try:
+#     json.loads(JsonString)
+# except JSONDecodeError:
+#     print("Could not parse JSON")
+
+# 
