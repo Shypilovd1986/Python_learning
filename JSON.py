6a25a34080a9c159de5e2050f6989856a7625e45
shypilovd1986 <shypilovd@gmail.com>
2023-06-22 21:52:07 +0300

    JSON-P

--- JSON.py
+++ JSON.py
@@ -13,4 +13,16 @@
 # except JSONDecodeError:
 #     print("Could not parse JSON")
 
-# 
+#  JSON-P stands for JSON with padding. JSON-P is a technique for fetching data for a webpage that's intended to work
+#  around some security limits that web browsers impose on web applications. One aspect of web security is based on
+#  origins, which you can think of as the web addresses associated with particular web apps. Web browsers prevent
+#  scripts from making requests to other origins. This is a same origin policy, and it's intended to prevent malware
+#  from injecting a request to another domain that accesses a user' sensitive information, it's a useful limitation.
+#  And on the modern web servers have many configuration options that enable them to work with selected other domains
+#  they trust. But earlier in the web's evolution, this same origin policy was a blunt tool. JSON-P was developed as
+#  a creative workaround. As part of the site's own code a statement is included that adds a script element to the page.
+#
+#And there are ways to accomplish everything that JSON-P can do while still maintaining security over all the data being
+# transmitted. The most common approach is CORS which includes using the access control allow origin header within your
+# HTTP requests and responses. For this reason it's best to you use these alternatives rather than relying on JSON-P.
+
