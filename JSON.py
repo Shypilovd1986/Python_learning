f51370bf56928f653dbc742aa99eb3ed6fdaa613
shypilovd1986 <shypilovd@gmail.com>
2023-06-26 21:55:54 +0300

    JSON-LD, JSON schema

--- JSON.py
+++ JSON.py
@@ -26,3 +26,42 @@
 # transmitted. The most common approach is CORS which includes using the access control allow origin header within your
 # HTTP requests and responses. For this reason it's best to you use these alternatives rather than relying on JSON-P.
 
+# JSON Schema is a standard for creating schemas for JSON data. It was created by the internet engineering task force.
+# To create a schema using JSON Schema, you create JSON using a specific structure and keywords along with values you
+# specify.
+
+#                                            JSON SCHEMA
+# The content of the schema is simply a set of keys and values, so a JavaScript object. By convention, I start with the
+# dollar schema keyword as the first key. And this makes it clear that this object is a JSON schema, rather than just
+# generic JSON. Remember that JSON requires key names as well as values to be double quoted.
+# this is a special value that's defined in the JSON schema specification.
+# productSchema = {
+#     "$schema": "http://json-schema.org/draft-07/schema#",
+#     "$id": "http://hplussport.com/schemas/product.json",
+#     "title": "h+ Sport products",
+#     "description": "schema for h+ sport product data",
+#     "type": "array",
+#     "items": {
+#         "type": "object",
+#         "properties": {
+#             "id": {
+#                 "type": "string"
+#             },
+#             "name": {
+#                 "type": "string"
+#             },
+#             "description": {
+#                 "type": "string"
+#             }
+#         },
+#         "required": ["id", "name", "description"]
+#     }
+# }
+
+# JSON-LD, which stands for JSON for linking data. JSON-LD follows the rules of JSON, but adds a couple standards to
+# convey the meaning of content. First, it references common vocabularies that are maintained by a variety of
+# organizations, including schema.org, productontology.org, and the W3C. Data built on the JSON-LD standard also
+# implements key names based on vocabularies that are references. Together, these two JSON-LD features enable an
+# application to parse data and make decisions about what it contains and how it should be used.
+
+#
