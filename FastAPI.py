07e8157af9fe204913d74b3eb40522e62456d702
shypilovd1986 <shypilovd@gmail.com>
2023-09-16 20:24:49 +0300

    about CORS and middleware

--- FastAPI.py
+++ FastAPI.py
@@ -4,3 +4,19 @@
 # asgi - Asynchronous Server Gateway Interface) — клиент-серверный протокол взаимодействия веб-сервера и приложения,
 # дальнейшее развитие технологии WSGI.
 #
+# If you are curious about how the raw OpenAPI schema looks like, FastAPI automatically generates a JSON (schema) with
+# the descriptions of all your API.
+#
+# You can see it directly at: http://127.0.0.1:8000/openapi.json
+
+
+#                                   CORS
+# Cross-Origin Resource Sharing (CORS) — механизм, использующий дополнительные HTTP-заголовки, чтобы дать возможность
+# агенту пользователя получать разрешения на доступ к выбранным ресурсам с сервера на источнике (домене), отличном от
+# того, что сайт использует в данный момент. Говорят, что агент пользователя делает запрос с другого источника
+# (cross-origin HTTP request), если источник текущего документа отличается от запрашиваемого ресурса доменом,
+# протоколом или портом.
+# https://developer.mozilla.org/ru/docs/Web/HTTP/CORS
+
+# https://fastapi.tiangolo.com/tutorial/cors/
+#https://fastapi.tiangolo.com/advanced/middleware/      about advanced middleware
